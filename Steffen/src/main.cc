81938223ba24cf3cebbb6dc44c60896f6b3ae7aa
Steffen Hinderink <shinderink@uni-osnabrueck.de>
2021-12-03 20:11:10 +0100

    3

--- Steffen/src/main.cc
+++ Steffen/src/main.cc
@@ -2,21 +2,23 @@
 #include <iostream>
 #include <vector>
 
-int aoc1() {
-    // std::ifstream file("input/1.txt");
-    // std::string line;
-    // std::getline(file, line);
-    // int last = std::stoi(line);
-    // int cnt = 0;
-    // while (std::getline(file, line)) {
-    //     int curr = std::stoi(line);
-    //     if (curr > last) {
-    //         cnt++;
-    //     }
-    //     last = curr;
-    // }
-    // return cnt;
+int aoc011() {
+    std::ifstream file("input/1.txt");
+    std::string line;
+    std::getline(file, line);
+    int last = std::stoi(line);
+    int cnt = 0;
+    while (std::getline(file, line)) {
+        int curr = std::stoi(line);
+        if (curr > last) {
+            cnt++;
+        }
+        last = curr;
+    }
+    return cnt;
+}
 
+int aoc012() {
     std::ifstream file("input/1.txt");
     std::string line;
     std::vector<int> input;
@@ -35,26 +37,28 @@ int aoc1() {
     return cnt;
 }
 
-int aoc2() {
-    // std::ifstream file("input/2.txt");
-    // std::string line;
-    // int horizontal = 0;
-    // int depth = 0;
-    // while (std::getline(file, line)) {
-    //     size_t pos = line.find(" ");
-    //     std::string direction = line.substr(0, pos);
-    //     line.erase(0, pos + 1);
-    //     int step = std::stoi(line);
-    //     if (direction == "forward") {
-    //         horizontal += step;
-    //     } else if (direction == "down") {
-    //         depth += step;
-    //     } else if (direction == "up") {
-    //         depth -= step;
-    //     }
-    // }
-    // return horizontal * depth;
+int aoc021() {
+    std::ifstream file("input/2.txt");
+    std::string line;
+    int horizontal = 0;
+    int depth = 0;
+    while (std::getline(file, line)) {
+        size_t pos = line.find(" ");
+        std::string direction = line.substr(0, pos);
+        line.erase(0, pos + 1);
+        int step = std::stoi(line);
+        if (direction == "forward") {
+            horizontal += step;
+        } else if (direction == "down") {
+            depth += step;
+        } else if (direction == "up") {
+            depth -= step;
+        }
+    }
+    return horizontal * depth;
+}
 
+int aoc022() {
     std::ifstream file("input/2.txt");
     std::string line;
     int horizontal = 0;
@@ -77,6 +81,91 @@ int aoc2() {
     return horizontal * depth;
 }
 
+int aoc031() {
+    std::ifstream file("input/3.txt");
+    std::string line;
+    std::vector<int> ones(12);
+    int cnt = 0;
+    while (std::getline(file, line)) {
+        for (size_t i = 0; i < 12; i++) {
+            if (line.at(i) == '1') {
+                ones[i]++;
+            }
+        }
+        cnt++;
+    }
+    int gamma = 0;
+    int epsilon = 0;
+    for (size_t i = 0; i < 12; i++) {
+        gamma <<= 1;
+        epsilon <<= 1;
+        if (ones[i] > cnt / 2) {
+            gamma += 1;
+        } else {
+            epsilon += 1;
+        }
+    }
+    return gamma * epsilon;
+}
+
+int aoc032() {
+    std::ifstream file("input/3.txt");
+    std::string line;
+    std::vector<std::vector<bool>> oxygen;
+    std::vector<std::vector<bool>> co2;
+    while (std::getline(file, line)) {
+        std::vector<bool> bits(12);
+        for (size_t i = 0; i < 12; i++) {
+            bits[i] = line.at(i) == '1';
+        }
+        oxygen.push_back(bits);
+        co2.push_back(bits);
+    }
+    for (size_t i = 0; i < 12; i++) {
+        int cnt = 0;
+        for (size_t j = 0; j < oxygen.size(); j++) {
+            if (oxygen[j][i]) {
+                cnt++;
+            }
+        }
+        bool most_common = cnt >= oxygen.size() - cnt;
+        for (size_t j = 0; j < oxygen.size(); j++) {
+            if (oxygen[j][i] != most_common) {
+                oxygen.erase(oxygen.begin() + j);
+                j--;
+            }
+        }
+    }
+    for (size_t i = 0; i < 12 && co2.size() > 1; i++) {
+        int cnt = 0;
+        for (size_t j = 0; j < co2.size(); j++) {
+            if (co2[j][i]) {
+                cnt++;
+            }
+        }
+        bool least_common = cnt < co2.size() - cnt;
+        for (size_t j = 0; j < co2.size(); j++) {
+            if (co2[j][i] != least_common) {
+                co2.erase(co2.begin() + j);
+                j--;
+            }
+        }
+    }
+    int gamma = 0;
+    int epsilon = 0;
+    for (size_t i = 0; i < 12; i++) {
+        gamma <<= 1;
+        epsilon <<= 1;
+        if (oxygen[0][i]) {
+            gamma += 1;
+        }
+        if (co2[0][i]) {
+            epsilon += 1;
+        }
+    }
+    return gamma * epsilon;
+}
+
 int main() {
-    std::cout << aoc2() << std::endl;
+    std::cout << aoc032() << std::endl;
 }
