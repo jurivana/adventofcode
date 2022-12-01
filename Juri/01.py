a3ce0fee2fe677f8c581a0c73c41c237b7f56588
Juri Vana <juri.vana@ipsyscon.de>
2022-12-01 15:40:26 +0100

    1

--- Juri/01.py
+++ Juri/01.py
@@ -1,18 +1,27 @@
 import sys
 import os
+import heapq
 from read import read
 from pathlib import Path
 
 file = Path(os.path.abspath(__file__)).stem
-input = read(file)
+input = read(file).split('\n\n')
 
 
 def part_1():
-    pass
+    max_calories = 0
+    for elf in input:
+        max_calories = max(max_calories, sum([int(calories) for calories in elf.splitlines()]))
+
+    return max_calories
 
 
 def part_2():
-    pass
+    calories = []
+    for elf in input:
+        heapq.heappush(calories, sum([int(calories) for calories in elf.splitlines()]))
+        
+    return sum(heapq.nlargest(3, calories))
 
 
 print(globals()[f'part_{sys.argv[1]}']())
