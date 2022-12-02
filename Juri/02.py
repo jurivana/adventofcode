c8856c9b4cef3ef765acb79a661acae40118cd2f
Juri Vana <juri.vana@ipsyscon.de>
2022-12-02 10:52:23 +0100

    2

--- Juri/02.py
+++ Juri/02.py
@@ -4,15 +4,34 @@ from read import read
 from pathlib import Path
 
 file = Path(os.path.abspath(__file__)).stem
-input = read(file)
+input = read(file).splitlines()
+
+
+def get_moves(round):
+    col1, col2 = [ord(move) for move in round.split(' ')]
+    col1 -= ord('A')
+    col2 -= ord('X')
+    return col1, col2
 
 
 def part_1():
-    pass
+    score = 0
+    for round in input:
+        opponent, me = get_moves(round)
+        me += 1
+        score += me + 3 * ((me - opponent) % 3)
+
+    return score
 
 
 def part_2():
-    pass
+    score = 0
+    for round in input:
+        opponent, result = get_moves(round)
+        me = (opponent + result - 1) % 3 + 1
+        score += me + 3 * result
+
+    return score
 
 
 print(globals()[f'part_{sys.argv[1]}']())
