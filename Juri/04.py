4bcda79ee996f7de5254d5921763fbe028ec34f9
Juri Vana <jvana@uos.de>
2022-12-04 17:44:12 +0100

    4

--- Juri/04.py
+++ Juri/04.py
@@ -4,15 +4,31 @@ from read import read
 from pathlib import Path
 
 file = Path(os.path.abspath(__file__)).stem
-input = read(file)
+input = read(file).splitlines()
 
+def get_ranges(line):
+    elf1, elf2 = line.split(',')
+    min1, max1 = map(int, elf1.split('-'))
+    min2, max2 = map(int, elf2.split('-'))
+    return min1, max1, min2, max2
 
 def part_1():
-    pass
+    count = 0
+    for line in input:
+        min1, max1, min2, max2 = get_ranges(line)
+        if (min1 <= min2 and max1 >= max2) or (min1 >= min2 and max1 <= max2):
+            count += 1
+
+    return count
 
 
 def part_2():
-    pass
+    count = 0
+    for line in input:
+        min1, max1, min2, max2 = get_ranges(line)
+        if max(min1, min2) <= min(max1, max2):
+            count += 1
+    return count
 
 
 print(globals()[f'part_{sys.argv[1]}']())
