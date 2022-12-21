98343c832c4010e31e36d1acc834880e465da1e8
Juri Vana <jvana@uos.de>
2022-12-21 22:20:39 +0100

    20

--- Juri/20.py
+++ Juri/20.py
@@ -4,15 +4,28 @@ from read import read
 from pathlib import Path
 
 file = Path(os.path.abspath(__file__)).stem
-input = read(file)
+input = read(file).splitlines()
+N = len(input)
+indices = list(range(N))
+
+
+def mix(numbers, times=1):
+    for i in indices * times:
+        idx = indices.index(i)
+        indices.insert((idx + numbers[i]) % (N - 1), indices.pop(idx))
+
+    zero = indices.index(numbers.index(0))
+    return sum(numbers[indices[(zero+p) % N]] for p in [1000, 2000, 3000])
 
 
 def part_1():
-    pass
+    numbers = [int(x) for x in input]
+    return mix(numbers)
 
 
 def part_2():
-    pass
+    numbers = [int(x) * 811589153 for x in input]
+    return mix(numbers, 10)
 
 
 print(globals()[f'part_{sys.argv[1]}']())
